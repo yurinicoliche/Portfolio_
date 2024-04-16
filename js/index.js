8478b60b97b7448cec2a6821e32045abf61ab19b
unknown <yurinicoliche2003@gmail.com>
2024-04-16 18:24:01 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto_inativo');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+
+    mostrarMaisProjetos();
+
+    esconderBotao();
+})
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add('remover');
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
